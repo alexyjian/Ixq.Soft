fa2c79e9de5997666d60c099a973343762da4330
赵坤 <zk9474@hotmail.com>
2018-06-03 16:25:55 +0800

    更新至 core 2.1

--- Ixq.Soft.Core/Domain/EntityBase.cs
+++ Ixq.Soft.Core/Domain/EntityBase.cs
@@ -4,14 +4,14 @@ namespace Ixq.Soft.Core.Domain
 {
     public class EntityBase : IEntityBase<int>, ICreationAudited, IUpdateAudited, ISoftDeleteAudited
     {
-        public int Id { get; set; }
-        public string SoteCode { get; set; }
-        public long CreationUserId { get; set; }
-        public DateTime CreationTime { get; set; }
-        public long UpdateUserId { get; set; }
-        public DateTime? UpdateTime { get; set; }
-        public long DeleteUserId { get; set; }
-        public DateTime? DeleteTime { get; set; }
-        public bool IsDeleted { get; set; }
+        public virtual int Id { get; set; }
+        public virtual string SoteCode { get; set; }
+        public virtual long CreationUserId { get; set; }
+        public virtual DateTime CreationTime { get; set; }
+        public virtual long UpdateUserId { get; set; }
+        public virtual DateTime? UpdateTime { get; set; }
+        public virtual long DeleteUserId { get; set; }
+        public virtual DateTime? DeleteTime { get; set; }
+        public virtual bool IsDeleted { get; set; }
     }
 }
